e8cdccbb07656592bb92438b4145243e76307094
AnugrahaTamang <tamanganugrah59@gmail.com>
2024-10-03 18:24:57 +0545

    variable

---
+++ Basic-Concept/Variable/app.js
@@ -0,0 +1,21 @@
+//Variable: variable is container which store the value. 
+// variable is also known as memory location. It store the differnt type of data.
+
+var a =  "anugraha tamang";
+console.log(a) //anugraha tamang
+var a = "ramit tamang"
+console.log(a) //ramit tamang
+//Note: var keyword is not good for variable declaration in js.
+let b = "ramit tamang"
+console.log(b) //ramit tamang
+b = "jeewan"
+console.log(b) //jeewan
+// let b = "anugraha tamang"
+console.log(b) //Error: it's not allowed in js
+
+const ab = 13
+console.log(ab)
+ab = 2 //error
+console.log(ab)
+
+
