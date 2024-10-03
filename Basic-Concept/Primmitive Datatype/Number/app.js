c39d6fc1e29a834e2567fe67261aa02ea1e58799
AnugrahaTamang <tamanganugrah59@gmail.com>
2024-10-03 20:34:52 +0545

    primittive datatype

---
+++ Basic-Concept/Primmitive Datatype/Number/app.js
@@ -0,0 +1,9 @@
+let a = 12;
+console.log(a)
+console.log(typeof(a))
+
+let b = 12.22
+console.log(b)
+console.log(typeof(b))
+
+console.log(a+b);
---
+++ Basic-Concept/Primmitive Datatype/String/app.js
@@ -0,0 +1,25 @@
+let name = "anugraha tamang"
+console.log(name)
+console.log(typeof name)
+
+// string interpolation
+name = `I am Anugraha tamang
+from Dhading Nepal.`
+console.log(name)
+
+//string concatination
+let fname = "ramit"
+let lname = "tamang"
+console.log(fname.concat(lname))
+console.log(fname.concat(" ram "))
+console.log(fname.includes('mit'))
+
+console.log(fname.indexOf("m"))
+console.log(fname.slice(2, 5))
+console.log(fname.length)
+console.log(fname.substring(2,4))
+let total = "     Do you know about the tittle  "
+console.log(total)
+console.log(total.trim())
+
+console.log(total.split(" ").join("-"))
