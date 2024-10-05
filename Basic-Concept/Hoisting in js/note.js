7fd6954c9c8fd5e64dc707495af4a43347987bf2
AnugrahaTamang <tamanganugrah59@gmail.com>
2024-10-05 11:54:02 +0545

    hosting

---
+++ Basic-Concept/Hoisting in js/note.js
@@ -0,0 +1,17 @@
+console.log(a) //undefined : note: it's called the hoisting here store the undefined.
+var a = 123
+console.log(a)
+
+// console.log(b); //"error"
+let b =123
+
+
+abb() // hello sir Note: this is called the function hosting. it's output is hello sir
+function abb(){
+    console.log("hello sir")
+}
+
+// abc() Note: error 
+let abc = function(){
+    console.log("I love Nepal")
+}
