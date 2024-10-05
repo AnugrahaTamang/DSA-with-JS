f3f67b0a4a9d5b940c38b7659ea9feae243ec73a
AnugrahaTamang <tamanganugrah59@gmail.com>
2024-10-05 11:54:55 +0545

    loop in js

---
+++ Basic-Concept/Loop in Javascript/note.js
@@ -0,0 +1,25 @@
+//for loop
+let a = 5;
+for(let i = 0; i<=a; i++){
+    console.log(i)
+}
+//sum of 5
+let sum = 0;
+for(let i =0; i<=a; i++){
+    console.log(sum+=i)
+}
+
+
+//while loop
+let i =0;
+while(i<=a){
+    console.log(i);
+    i++;
+}
+
+let g = 2;
+do{
+    console.log(g);
+    g++;
+}
+while(g<=6)
