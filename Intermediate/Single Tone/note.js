f9ced93ded726f6df45a61de6d97dfe7d25d77ac
AnugrahaTamang <anugrah59@gmail.com>
2024-10-09 16:35:58 +0545

    single tone

---
+++ Intermediate/Single Tone/note.js
@@ -0,0 +1,12 @@
+//Object single tone
+let person = {};
+let obj = Object.create(person);
+obj.name = "bijay";
+obj.age = 22;
+obj.location = "japan";
+console.log(obj);
+
+
+//Array single tone
+const arr = Array(1,2,3,4)
+console.log(arr)
