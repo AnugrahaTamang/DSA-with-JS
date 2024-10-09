e6ad9f9babdfbb53a874f1b19c32f58071c12290
AnugrahaTamang <anugrah59@gmail.com>
2024-10-09 16:35:31 +0545

    literals

---
+++ Intermediate/Literals/note.js
@@ -0,0 +1,19 @@
+// Object literals
+const obj = {
+  name: "anu",
+  age: 22,
+};
+console.log(obj);
+
+//Array literals
+const arr = ["anu", "ramit", 22, "ktm"];
+console.log(arr);
+
+//String literals
+let name = "jeewan tamang";
+console.log(name);
+
+//Template Literals
+name = `I am 
+bijay tamang from dhading Nepal`;
+console.log(name);
