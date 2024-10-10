158727c1515e0115ac43c2dd0317200d90b00619
AnugrahaTamang <anugrah59@gmail.com>
2024-10-10 17:06:15 +0545

    what is the DOM

---
+++ DOM/What is the DOM/note.js
@@ -0,0 +1,6 @@
+//Document object model is one of important part of learning the JS. When we rander the html file into browser. browser automatically create the DOM.
+console.log(document);
+console.log(document.URL);
+console.log(document.location);
+console.log(document.head);
+console.log(document.title);
