7ed3ba53d4bfa850f4e04b9e10fc91fc806744b1
AnugrahaTamang <anugrah59@gmail.com>
2024-10-12 11:41:58 +0545

    accessing the dom

---
+++ DOM/Accessing the DOM/app.js
@@ -0,0 +1,9 @@
+console.log(document.getElementsByTagName("h1"));
+console.log(document.getElementsByClassName("one"));
+console.log(document.getElementById("one"));
+//about the queryselector
+console.log(document.querySelector("h2"));
+console.log(document.querySelector("h2.one"));
+console.log(document.querySelector("#two"))
+
+console.log(document.querySelectorAll("h1"));
