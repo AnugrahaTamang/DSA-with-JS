45617850fb445ec4e227ce76cbd8897f39130789
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-01-17 20:08:31 +0545

    set

--- Intermediate/Sets/note.js
+++ Intermediate/Sets/note.js
@@ -19,3 +19,35 @@ console.log(newset); //it remove the double element
 for (const key of newset) {
   console.log(key);
 }
+/* There are different type of set method:
+1. add()
+2. has()
+3. delete()
+4. values()
+5. keys()
+6. clear()
+7. next()
+8. entries()
+//Method used in set
+1. forEach()
+2. for-of
+*/
+
+const setone = new Set([12,13,14])
+console.log(setone)
+setone.add(20)
+console.log(setone)
+console.log(setone.has(12))
+console.log(setone.delete(12))
+console.log(setone)
+const dataone = setone.forEach((keys,values) => {
+  console.log(`${keys} : ${values}`)
+})
+console.log(dataone)
+const datatwo = setone.entries()
+console.log(datatwo.next.values)
+console.log(datatwo.next.values)
+
+const datafour = setone.values()
+console.log(datafour.next().value) //13
+console.log(datafour.next().value) //14
