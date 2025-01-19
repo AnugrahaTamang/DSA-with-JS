fe48cad2d120424ec26ad3648a4222eed62a9f0b
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-01-19 11:04:20 +0545

    prototype

--- OOP Concept in JS/Prototype/index.js
+++ OOP Concept in JS/Prototype/index.js
@@ -1,5 +1,6 @@
 /* The Object.create() method creates a new object, using aan existing object as the 
 prototype of the newly created object.
+Note: Every object in JavaScript has a built-in property, which is called its prototype
 */
 let person = {
     greet: function(){
