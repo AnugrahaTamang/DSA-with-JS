b4f8a793309cad26bca7dd39f081f742f1b3e9b3
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-03-30 13:46:02 +0545

    array challenge

---
+++ DSA/fun/app.js
@@ -0,0 +1,14 @@
+console.log(x===undefined); //true
+var x =12;
+(function(){
+    console.log(x);
+    var x = "local value"
+})() //undefined
+
+const myList = [
+    "home",
+    "school",
+  + "hospital",
+  ];
+  console.log(myList)
+
