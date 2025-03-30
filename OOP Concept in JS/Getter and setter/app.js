250260dc42531188eb431a94274cca08a9c23d0d
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-03-30 15:42:12 +0545

    get and set

--- OOP Concept in JS/Getter and setter/app.js
+++ OOP Concept in JS/Getter and setter/app.js
@@ -1,17 +1,18 @@
-class User{
-    constructor(name, password){
-        this.name = name;
-        this.password = password;
-    }
-    //get the value from the class
-    get password(){
-        return `${this._password}tamang`
-    }
-    //set the value inside the class but we can not return the value inside the set.
-    set password(value){
-        this._password = value
-    }
+class User {
+  constructor(name, password) {
+    this.name = name;
+    this.password = password;
+  }
+  //get the value from the class
+  get password() {
+    return `${this._password}tamang`;
+  }
+  //set the value inside the class but we can not return the value inside the set.
+  set password(value) {
+    this._password = value;
+  }
 }
 
-const anugraha = new User("anugraha.ai", "abc")
-console.log(anugraha.password)
+const anugraha = new User("anugraha.ai", "abc");
+
+console.log(anugraha.password);
